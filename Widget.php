99518c5748e5e398f76eccb3df8c450a14a14eb4
alexeevdv <mail@alexeevdv.ru>
2015-09-22 12:57:23 +0300

    Email hashing

--- Widget.php
+++ Widget.php
@@ -4,7 +4,22 @@ namespace alexeevdv\gravatar;
 
 class Widget extends \yii\base\Widget
 {
+    public $secure = true;
+
+    public $email = false;
+
+    public $size;
+
+    const URL_HTTPS = "https://secure.gravatar.com/avatar/";
+
+    const URL_HTTP = "http://www.gravatar.com/avatar/";
+
     public function run()
     {
+
+        $hash = md5(strtolower(trim($this->email)));
+      
+        return ($this->secure ? self::URL_HTTPS : self::URL_HTTP).$hash;
+
     }
 }
